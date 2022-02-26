e419feb9e0eeef0431aead54f627149dc81d7cbe
Zhendong Tang <zhendongtt@gmail.com>
2022-02-26 17:17:12 -0500

    2-4. Add Register View

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            var registerViewModel = new RegisterViewModel();
+            return View(registerViewModel); 
+        }
+    }
+}
