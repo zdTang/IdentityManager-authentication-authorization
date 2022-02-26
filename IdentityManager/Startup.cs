5f893548495c419a43e2805de032fcd2efcd77e3
Zhendong Tang <zhendongtt@gmail.com>
2022-02-26 12:13:01 -0500

    1-14-Setup DbContext
    Add necessary Packages
    Configure Connection String
    Dependency Injection--configure DbContext

--- IdentityManager/Startup.cs
+++ IdentityManager/Startup.cs
@@ -1,6 +1,8 @@
+using IdentityManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,16 +15,17 @@ namespace IdentityManager
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
+        }   
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options=>options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
