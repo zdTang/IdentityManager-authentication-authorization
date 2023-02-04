4401417c2a5a2413f727ce51f689e771a7412d59
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-02-04 17:16:57 -0500

    3-4 Setup MailJet Email Sender

---
+++ IdentityManager/Services/MailJetEmaiSender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace IdentityManager.Services
+{
+    public class MailJetEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        MailJetEmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+       }
+    
+        //https://dev.mailjet.com/email/guides/send-api-V3/#send-api-v3-to-v3.1
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient(_configuration["MailJet:ApiKey"], _configuration["MailJet:SecretKey"]);
+            MailjetRequest request = new MailjetRequest
+                {
+                    Resource = Send.Resource,
+                }
+                .Property(Send.FromEmail, "pilot@mailjet.com")
+                .Property(Send.FromName, "Mailjet Pilot")
+                .Property(Send.Subject, subject)
+                .Property(Send.TextPart, "Dear passenger, welcome to Mailjet! May the delivery force be with you!")
+                .Property(Send.HtmlPart, htmlMessage)
+                .Property(Send.Recipients, new JArray {
+                    new JObject {
+                        {"Email", email}
+                    }
+                });
+           // MailjetResponse response = await client.PostAsync(request);
+            await client.PostAsync(request); // We don't need the response
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+            //    Console.WriteLine(response.GetData());
+            //}
+            //else
+            //{
+            //    Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+            //    Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+            //    Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+            //}
+        }
+    }
+}
---
+++ IdentityManager/Services/MailJetOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityManager.Services
+{
+    public class MailJetOptions
+    {
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
